dcf3754585617a6e9552c22c2fbad220f6bfcd59
reetze <elreetz0@chicagobooth.edu>
2020-03-03 00:54:24 +0000

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,7 @@
 class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
+  validates :phone_number, :presence => true
+  
   has_secure_password
 end
